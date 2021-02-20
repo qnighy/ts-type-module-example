cda5ced1b1f2d111c6d6b7ffb06d56c842d022e0
Masaki Hara <ackie.h.gmai@gmail.com>
2021-02-20 18:56:05 +0900

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+export default {
+  "mode": "development",
+  "context": path.resolve(__dirname, "./src"),
+  "target": "node",
+  "entry": "./index",
+  "resolve": {
+    "extensions": ['.wasm', '.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  "output": {
+    "path": path.resolve(__dirname, "./dist-webpack"),
+    // "chunkFormat": "module", // Not implemented yet
+  },
+  "module": {
+    "rules": [
+      {
+        test: /\.[tj]sx?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-typescript"],
+            plugins: [
+              ["replace-import-extension", { "extMapping": { ".js": "" }}],
+            ],
+          },
+        },
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
+    ],
+  },
+};
