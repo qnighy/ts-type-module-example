f0b8bdb23e5e3f73cc461282a8ac3b60645252f2
Masaki Hara <ackie.h.gmai@gmail.com>
2021-02-21 15:32:26 +0900

    Add example testing modules importing another module

--- src/square.test.ts
+++ src/square.test.ts
@@ -1,3 +1,4 @@
+// TODO: use ./square.js
 import { square } from "./square";
 
 describe("square", () => {
---
+++ src/square42.test.ts
@@ -0,0 +1,10 @@
+// TODO: use ./square42.js
+import { square42 } from "./square42";
+
+describe("square42", () => {
+  it("returns 1764", () => {
+    expect(square42()).toBe(1764);
+  });
+});
+
+export type T = 42;
