f206ec79404c088fd6ba142b19de0d1240d1dbb9
Masaki Hara <ackie.h.gmai@gmail.com>
2021-02-21 11:17:21 +0900

    Use jest 27

--- jest-ts.config.ts
+++ jest-ts.config.ts
@@ -2,5 +2,8 @@ export default {
   transform: {
     "\\.[jt]sx?$": "ts-jest",
   },
+  // See https://github.com/facebook/jest/pull/10823.
+  // We don't need this with Jest 26 + ts-jest
+  extensionsToTreatAsEsm: [".ts"],
   rootDir: "./src",
 };
